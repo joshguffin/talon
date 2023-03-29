7c59678ca3991a4d2bba747600338a627b868e21
Josh Guffin <josh.guffin@gmail.com>
2023-03-29 08:48:09 -0400

    Add browser shortcuts

---
+++ browser.talon
@@ -0,0 +1,4 @@
+tag: browser
+-
+
+check: user.rango_command_without_target("toggleHints")
---
+++ setup.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/knausj85/knausj_talon knausj_talon
+git clone https://github.com/wolfmanstout/talon-gaze-ocr
+git clone https://github.com/david-tejada/rango-talon
